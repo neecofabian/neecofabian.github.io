ca5dbbe22509b5e2c3c877885be051ac8e682f71
neecofabian <neecofabian@gmail.com>
2021-05-05 18:50:36 -0400

    Restore colours, hover on courses

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ $('.course').width($('.marquee-left1').width());
 //     selector: ".project",
 //     shine: true
 // });
-// if (!(window.matchMedia("only screen and (max-width: 750px)").matches)) {
-//     pivot.init({
-//         selector: ".course",
-//         shine: true
-//     });
-// }
+if (!(window.matchMedia("only screen and (max-width: 750px)").matches)) {
+    pivot.init({
+        selector: ".course",
+        shine: true
+    });
+}
