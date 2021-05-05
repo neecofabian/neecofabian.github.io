b319966ab57a03132dda1cdac947fd6c6e35ae21
neecofabian <neecofabian@gmail.com>
2021-05-05 18:06:59 -0400

    Test coloured courses, pivot.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 $('.course').width($('.marquee-left1').width());
+
+// pivot.init({
+//     selector: ".project",
+//     shine: true
+// });
+
+pivot.init({
+    selector: ".course",
+    shine: true
+});
