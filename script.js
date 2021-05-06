93a3c77df01c558696f38167a7166372b9f6447d
neecofabian <neecofabian@gmail.com>
2021-05-05 20:13:55 -0400

    Fix resizing issue for course

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 $('.course').width($('.marquee-left1').width());
 
-// pivot.init({
-//     selector: ".project",
-//     shine: true
-// });
 if (!(window.matchMedia("only screen and (max-width: 750px)").matches)) {
     pivot.init({
         selector: ".course",
