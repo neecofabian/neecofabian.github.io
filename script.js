fe621af91eb57441bcbaa43d1814879b25c4ad33
neecofabian <neecofabian@gmail.com>
2021-05-06 22:59:02 -0400

    Turn off extra box shadows temporarily

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ $('.course').width($('.marquee-left1').width());
 if (!(window.matchMedia("only screen and (max-width: 750px)").matches)) {
     pivot.init({
         selector: ".course",
-        shine: true
+        shine: true,
     });
 }
