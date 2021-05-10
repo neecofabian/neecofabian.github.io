109915ee8efbbe06188283b9b80fccc31d223d94
neecofabian <neecofabian@gmail.com>
2021-05-10 04:32:57 -0400

    Add alternate tools section, persistent section titles

--- script.js
+++ script.js
@@ -7,17 +7,18 @@ if (!(window.matchMedia("only screen and (max-width: 750px)").matches)) {
     });
 }
 
-gsap.registerPlugin("ScrollTrigger");
-gsap.to(".toolbox-section", {
-    scrollTrigger: {
-        trigger: ".toolbox-section",
-        start: "top 20%",
-        pin: ".toolbox-section",
-        scrub: true,
-        sensitivity: 100
-        // markers: true
-    }
-})
+// UNCOMMENT LATER
+// gsap.registerPlugin("ScrollTrigger");
+// gsap.to(".toolbox-section", {
+//     scrollTrigger: {
+//         trigger: ".toolbox-section",
+//         start: "top 20%",
+//         pin: ".toolbox-section",
+//         scrub: true,
+//         sensitivity: 100
+//         // markers: true
+//     }
+// })
 
 // gsap.to(".tool", {
 //     y: '300%',
@@ -30,30 +31,31 @@ gsap.to(".toolbox-section", {
 //     }
 // })
 
-const tools = gsap.utils.toArray('.tool');
-tools.reverse().forEach(tool => {
-  gsap.to(tool, { // this will animate ALL boxes
-    y: '300%',
-    scrollTrigger: {
-        trigger: tool,
-        start: "top center",
-        end: "+=100%",
-        scrub: true,
-        toggleClass: {targets: tool, className: "tool-seen"},
-        markers: true
-    }
-  })
-});
+// UNCOMMENT LATER
+// const tools = gsap.utils.toArray('.tool');
+// tools.reverse().forEach(tool => {
+//   gsap.to(tool, { // this will animate ALL boxes
+//     // y: '300%',
+//     scrollTrigger: {
+//         trigger: tool,
+//         start: "top center",
+//         end: "+=100%",
+//         scrub: true,
+//         toggleClass: {targets: tool, className: "tool-seen"},
+//         // markers: true
+//     }
+//   })
+// });
 
-gsap.to(".section-title", {
-    scrollTrigger: {
-        trigger: ".section-title",
-        start: "top top",
-        // end: () => `+=${document.querySelector(".programmer").offsetHeight}`,
-        // scrub: true,
-        pin: true,
-        // toggleClass: ".tool-seen",
-        // pinSpacing: false,
-        markers: true
-    }
-  })
+// gsap.to(".section-title", {
+//     scrollTrigger: {
+//         trigger: ".section-title",
+//         start: "top top",
+//         // end: () => `+=${document.querySelector(".programmer").offsetHeight}`,
+//         // scrub: true,
+//         pin: true,
+//         // toggleClass: ".tool-seen",
+//         pinSpacing: false,
+//         // markers: true
+//     }
+//   })
