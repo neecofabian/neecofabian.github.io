47d929b75a1b892d685741b4586762a1d1c0a099
neecofabian <neecofabian@gmail.com>
2021-05-22 21:03:28 -0400

    Chevron fade on scroll, js housekeeping, safari favicon resize, try to fix safari flap issue

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-$('.course').width($('.marquee-left1').width());
+$('.course').width($('.marquee-left1').width()); // for marquee
 
+// Pivot courses when not on mobile
 if (!(window.matchMedia("only screen and (max-width: 750px)").matches)) {
     pivot.init({
         selector: ".course",
@@ -7,66 +8,25 @@ if (!(window.matchMedia("only screen and (max-width: 750px)").matches)) {
     });
 }
 
-// UNCOMMENT LATER
 gsap.registerPlugin("ScrollTrigger");
-// gsap.to(".toolbox-section", {
-//     scrollTrigger: {
-//         trigger: ".toolbox-section",
-//         start: "top 20%",
-//         pin: ".toolbox-section",
-//         scrub: true,
-//         sensitivity: 100
-//         // markers: true
-//     }
-// })
 
-// gsap.to(".tool", {
-//     y: '300%',
-//     scrollTrigger: {
-//         trigger: ".toolbox-section",
-//         start: "top center",
-//         end: "bottom end",
-//         scrub: true,
-//         markers: true
-//     }
-// })
-
-// UNCOMMENT LATER
+// Show each tool
 const tools = gsap.utils.toArray('.tool');
-tools.reverse().forEach(tool => {
-  gsap.to(tool, { // this will animate ALL boxes
-    // y: '300%',
+tools.forEach(tool => {
+  gsap.to(tool, {
     scrollTrigger: {
         trigger: tool,
         start: "top center+=5%",
         // end: "+=150%",
         scrub: true,
         toggleClass: {targets: tool, className: "tool-seen"},
-        // markers: true
     }
   })
 });
 
-// gsap.to(".section-title", {
-//     scrollTrigger: {
-//         trigger: ".section-title",
-//         start: "top top",
-//         // end: () => `+=${document.querySelector(".programmer").offsetHeight}`,
-//         // scrub: true,
-//         pin: true,
-//         // toggleClass: ".tool-seen",
-//         pinSpacing: false,
-//         // markers: true
-//     }
-//   })
-
+// Flip the flap
 var timeline = gsap.timeline();
-
-timeline.to(".flap",
-{
-    rotationX: 180,
-})
-
+timeline.to(".flap", { rotationX: 180, })
 ScrollTrigger.create({
     trigger: ".tools",
     animation: timeline,
@@ -74,19 +34,20 @@ ScrollTrigger.create({
     end: "top center-=15%",
     scrub: true,
     // toggleClass: {targets: ".flap-text", className: "flap-text-seen"}
-    // markers: true
 })
 
-// gsap.to(".flap", {
-//         rotateX: 170,
-//         scrollTrigger: {
-//             trigger: ".toolbox-section",
-//             start: "top center", 
-//             // end: () => `+=${document.querySelector(".programmer").offsetHeight}`,
-//             scrub: true,
-//             // pin: true,
-//             // toggleClass: ".flap-move",
-//             // pinSpacing: false,
-//             // markers: true
-//         }
-//       })
+gsap.to(".arrow", {
+    opacity: 0,
+    // duration: 0.4,
+    scrollTrigger: {
+        trigger: ".arrow",
+        start: "top center+=10%",
+        end: "top center-=10%",
+        scrub: true,
+        marker: true
+    }
+  })
+
+// end: () => `+=${document.querySelector(".programmer").offsetHeight}`,
+// toggleClass: ".tool-seen",
+// pinSpacing: false,
